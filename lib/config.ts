8d0819e8da8ce8a87e48f768a76678da03cac52e
cjmaret <cjmaret@gmail.com>
2025-05-31 15:13:11 -0600

    fix: update config

--- lib/config.ts
+++ lib/config.ts
@@ -1,14 +1,5 @@
-const apiUrls = {
-  // local: 'http://192.168.1.104:8000',
-  local: 'https://api.aurelialabs.net',
-  droplet: 'http://24.144.89.186:8000',
-  production: 'https://api.aurelialabs.net',
-};
-
-const ENV = process.env.EXPO_PUBLIC_ENV || 'production';
-
 const config = {
-  apiUrl: apiUrls[ENV as keyof typeof apiUrls] || apiUrls.production,
+  apiUrl: 'https://api.aurelialabs.net',
 };
 
 export default config;
