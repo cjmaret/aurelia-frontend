a2934fcb1d5060a2011bc8dcaad01c80f0cd0f4e
cjmaret <cjmaret@gmail.com>
2025-06-07 00:44:04 -0600

    test: backend api

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,8 @@
 const config = {
   apiUrl: 'https://api.aurelialabs.net',
+  // apiUrl: 'http://192.168.1.104:8000',
 };
+
+console.log('API URL in build:', config.apiUrl);
 
 export default config;
