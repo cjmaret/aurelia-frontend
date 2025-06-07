fdf2831afc816c4ba1d5e3e5611664bdcbacc831
cjmaret <cjmaret@gmail.com>
2025-06-07 01:16:53 -0600

    commit already committed changes for some reason

--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,5 @@
 const config = {
   apiUrl: 'https://api.aurelialabs.net',
-  // apiUrl: 'http://192.168.1.104:8000',
 };
-
-console.log('API URL in build:', config.apiUrl);
 
 export default config;
