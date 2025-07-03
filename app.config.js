303c43b931d60003ad106721d591e18e886c13c2
cjmaret <cjmaret@gmail.com>
2025-07-03 01:15:46 -0600

    bump version number to 1.0.4

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: 'Aurelia',
     slug: 'aurelia-frontend',
     owner: 'cjmaret',
-    version: '1.0.3',
+    version: '1.0.4',
     orientation: 'portrait',
     icon: './assets/images/aurelia-icon.png',
     scheme: 'com.aureliaai.myapp',
