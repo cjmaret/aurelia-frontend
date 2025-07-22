9d2c3f888df8ac1f70a25ffde3cf8ba8f777a2da
cjmaret <cjmaret@gmail.com>
2025-07-22 10:49:44 -0600

    feat: enable proguard to reduce apk size

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ export default {
         backgroundColor: '#ffffff',
       },
       package: 'com.cjmaret.aureliafrontend',
-      versionCode: 31,
+      versionCode: 32,
       versionName: '1.0.5',
       permissions: [
         'android.permission.INTERNET',
